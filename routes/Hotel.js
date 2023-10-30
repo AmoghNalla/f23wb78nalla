26b4d5968efa21f6bdde51d5a4d43fba75743f3a
AmoghNalla <s566278@nwmissouri.edu>
2023-10-29 23:47:31 -0500

    changed the class name and its attributed and specified the same in readme file

--- routes/Hotel.js
+++ routes/Hotel.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Search Results House' });
+  res.render('index', { title: 'Search Results Hotel' });
 });
 
 module.exports = router;
