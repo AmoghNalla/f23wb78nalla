61fd2146ec0d1c5a5b22d5f6f3a54e9ab7208eae
AmoghNalla <s566278@nwmissouri.edu>
2023-10-31 14:25:49 -0500

    made some changes to the pug files

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var hotelRouter = require('./routes/Hotel');
 var boardRouter = require('./routes/board');
+var chooseRouter= require('./routes/choose');
+
+
 
 var app = express();
 
@@ -25,6 +28,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Hotel', hotelRouter );
 app.use('/board', boardRouter);
+app.use('/choose',chooseRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
