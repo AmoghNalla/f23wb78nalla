bd67a2fbb1c1b02eda84c3670cbc227ed9d00c5f
AmoghNalla <s566278@nwmissouri.edu>
2023-10-31 15:51:27 -0500

    changed file names of hotel to hotel

--- routes/hotel.js
+++ routes/hotel.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Hotel', { title: 'Search Results Hotel' });
+  res.render('hotel', { title: 'Search Results Hotel' });
 });
 
 module.exports = router;
